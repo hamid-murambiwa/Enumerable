d91c36cc7d850d1e2b5ebc17cb25bf315c76a7d8
MCKENT05 <atemitope95@gmail.com>
2022-01-21 14:32:56 +0100

    corrected errors and added readme

--- enumerable.rb
+++ enumerable.rb
@@ -1,31 +1,31 @@
 module MyEnumerable
-    def all?
-        return unless block_given?
-    
-        result = true
-        list.each do |e|
-          result = false unless yield(e)
-        end
-        result
-      end
-    
-      def any?
-        return unless block_given?
-    
-        result = false
-        list.each do |e|
-          result = true if yield(e)
-        end
-        result
-      end
-    
-      def filter
-        return unless block_given?
-    
-        listArr = []
-        list.each do |e|
-          listArr.push(e) if yield(e)
-        end
-        listArr
-      end
+  def all?
+    return unless block_given?
+
+    result = true
+    @list.each do |e|
+      result = false unless yield(e)
+    end
+    result
   end
+
+  def any?
+    return unless block_given?
+
+    result = false
+    @list.each do |e|
+      result = true if yield(e)
+    end
+    result
+  end
+
+  def filter
+    return unless block_given?
+
+    list_arr = []
+    @list.each do |e|
+      list_arr.push(e) if yield(e)
+    end
+    list_arr
+  end
+end
---
+++ list.rb
@@ -0,0 +1,29 @@
+require_relative 'enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+end
+
+# Verify solution:
+# Create our list
+puts list = MyList.new(1, 2, 3, 4)
+# <MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+# true
+puts(list.all? { |e| e > 5 })
+# false
+
+# Test #any?
+puts(list.any? { |e| e == 2 })
+# true
+puts(list.any? { |e| e == 5 })
+# false
+
+# Test #filter
+puts(list.filter(&:even?))
+# [2, 4]
